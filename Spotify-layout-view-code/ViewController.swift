95785911539ed5e91e81502f6a8bb46a106c3f92
Luciano Berchon <berchon@gmail.com>
2022-07-22 20:31:01 -0300

    Initial Commit

---
+++ Spotify-layout-view-code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify-layout-view-code
+//
+//  Created by Luciano Berchon on 22/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
